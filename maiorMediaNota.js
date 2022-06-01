03a464b1003228c60e4b2e681e6c5a81a329e297
rmcstt <anisio.nanato@hotmail.com>
2022-06-01 12:03:01 -0300

    .

--- maiorMediaNota.js
+++ maiorMediaNota.js
@@ -14,7 +14,9 @@ MelhorEstudante = estudantes => {
 }
 
 MelhorEstudante({
+  ///
   Joao: [8, 7.6, 8.9, 6],
   Mariana: [9, 6.6, 7.9, 8],
   Carla: [7, 7, 8, 9]
 })
+///{ nome: 'Mariana', media: '7.88' }
